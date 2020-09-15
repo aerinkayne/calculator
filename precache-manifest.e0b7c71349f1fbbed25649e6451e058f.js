05f0f5efdd17c6f2bd978fefb283f00b0a1c220c
Carrie <chanscom7@gmail.com>
2020-09-15 13:41:26 -0400

    Updates

---
+++ precache-manifest.e0b7c71349f1fbbed25649e6451e058f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89764c44f5a994a63416163b219be382",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bc786ff4b42fd48f2497",
+    "url": "/calculator/static/css/main.25072a2f.chunk.css"
+  },
+  {
+    "revision": "f2602c148524fd7db66c",
+    "url": "/calculator/static/js/2.4ce8e436.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.4ce8e436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc786ff4b42fd48f2497",
+    "url": "/calculator/static/js/main.e72abdad.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
